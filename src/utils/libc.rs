ae5375dd757e1780fa6d10ce1c2730c481d50794
Baptiste Covolato <b.covolato@gmail.com>
2015-10-19 22:03:25 +0200

    libc: Add memset

--- src/utils/libc.rs
+++ src/utils/libc.rs
@@ -9,3 +9,15 @@ pub unsafe fn memcpy(dst: *mut u8, src: *const u8, n: usize) -> *mut u8 {
 
     dst
 }
+
+#[no_mangle]
+pub unsafe fn memset(ptr: *mut u8, value: i32, n: usize) -> *mut u8 {
+    let mut i = 0;
+
+    while i < n {
+        *ptr.offset(i as isize) = value as u8;
+        i += 1;
+    }
+
+    ptr
+}
