0bd81261304749999c49b075be37b344af9accb7
Baptiste Covolato <b.covolato@gmail.com>
2015-10-20 14:36:14 +0200

    build.rs: Add detection of the target architecture

--- build.rs
+++ build.rs
@@ -4,6 +4,17 @@ use std::fs;
 use std::env;
 use std::path::Path;
 
+fn get_target() -> Option<&'static str> {
+    let env_target = env::var("TARGET").unwrap();
+    let v: Vec<&str> = env_target.split('-').collect();
+
+    match *v.first().unwrap() {
+        "i686" => Some("x86"),
+        "x86_64" => Some("x86_64"),
+        _ => None,
+    }
+}
+
 fn build_libuniarch(arch_path: &std::path::PathBuf) {
     let arch_content = fs::read_dir(arch_path).unwrap();
     let mut gcc_config = gcc::Config::new();
@@ -21,7 +32,10 @@ fn build_libuniarch(arch_path: &std::path::PathBuf) {
 }
 
 fn main() {
-    let target = "x86";
+    let target = match get_target() {
+        Some(t) => t,
+        None => panic!("Unable to determine the target arch"),
+    };
     let out_dir = env::var("OUT_DIR").unwrap();
     let out_dir_path = Path::new(&out_dir[..]);
     let arch_path = Path::new("src/arch/").join(target);
