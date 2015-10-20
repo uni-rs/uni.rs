58519b2f599b6b17c75d228758b7b14a8ed66d85
Baptiste Covolato <b.covolato@gmail.com>
2015-10-20 13:12:48 +0200

    misc: Move src/arch/i686 to src/arch/x86

--- build.rs
+++ build.rs
@@ -8,10 +8,10 @@ fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
     let prefix = Path::new(".");
 
-    fs::copy(prefix.join("src/arch/i686/linker.ld"),
+    fs::copy(prefix.join("src/arch/x86/linker.ld"),
              out_dir.clone() + "/linker.ld").unwrap();
 
-    gcc::compile_library("libuniarch.a", &["src/arch/i686/boot.S"]);
+    gcc::compile_library("libuniarch.a", &["src/arch/x86/boot.S"]);
 
     println!("cargo:rustc-link-search=native={}", out_dir);
 }
