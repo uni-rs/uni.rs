053dfadda84090246cc6b412eba7ac5c4519df0f
Baptiste Covolato <b.covolato@gmail.com>
2015-10-21 14:21:13 +0200

    arch: Add module x86_common

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -1,11 +1,14 @@
-#[cfg(target_arch = "x86")]
-#[path="x86"]
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+pub mod x86_common;
+
+#[cfg(target_arch = "x86_64")]
+#[path="x86_64"]
 mod imp {
     pub mod types;
 }
 
-#[cfg(target_arch = "x86_64")]
-#[path="x86_64"]
+#[cfg(target_arch = "x86")]
+#[path="x86"]
 mod imp {
     pub mod types;
 }
