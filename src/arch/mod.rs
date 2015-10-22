cd1a9528b11f81c653ffaa30aaab96e34f63bacc
Baptiste Covolato <b.covolato@gmail.com>
2015-10-22 23:17:24 +0200

    arch: Rename module types to defs

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -4,16 +4,16 @@ pub mod x86_common;
 #[cfg(target_arch = "x86_64")]
 #[path="x86_64"]
 mod imp {
-    pub mod types;
+    pub mod defs;
     pub use ::arch::x86_common::barrier;
 }
 
 #[cfg(target_arch = "x86")]
 #[path="x86"]
 mod imp {
-    pub mod types;
+    pub mod defs;
     pub use ::arch::x86_common::barrier;
 }
 
-pub use self::imp::types;
+pub use self::imp::defs;
 pub use self::imp::barrier;
