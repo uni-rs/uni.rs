cd05706e15338289ddfa5ce4dc7e9bff320b643c
Baptiste Covolato <b.covolato@gmail.com>
2015-10-25 21:17:18 +0100

    sched: Add poweroff() function

--- src/xen/sched.rs
+++ src/xen/sched.rs
@@ -15,10 +15,45 @@ enum SchedOp {
 }
 
 #[allow(dead_code)]
+enum ShutdownReason {
+    PowerOff = 0,
+    Reboot = 1,
+    Suspend = 2,
+    Crash = 3,
+    Watchdog = 4,
+}
+
+#[repr(C)]
+struct SchedShutdown {
+    reason: u32,
+}
+
 fn sched_op(op: SchedOp, sched: Ulong) -> i32 {
     hypercall2(HyperCalls::SchedOp, op as Ulong, sched) as i32
 }
 
 pub fn yield_cpu() -> i32 {
     sched_op(SchedOp::Yield, 0)
+}
+
+fn shutdown(reason: ShutdownReason) -> i32 {
+    let r = SchedShutdown {
+        reason: reason as u32,
+    };
+
+    sched_op(SchedOp::Shutdown, &r as *const SchedShutdown as Ulong)
+}
+
+fn shutdown_code(status: Ulong) -> i32 {
+    sched_op(SchedOp::ShutdownCode, status)
+}
+
+pub fn poweroff(status: Ulong) -> i32 {
+    let ret: i32 = shutdown_code(status);
+
+    if ret < 0 {
+        return ret
+    }
+
+    shutdown(ShutdownReason::PowerOff)
 }
