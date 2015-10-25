d58946887e60576a3e42cf33b7397583dd3701a1
Baptiste Covolato <b.covolato@gmail.com>
2015-10-25 19:35:21 +0100

    x86_common: Add definition of global start_info

--- src/arch/x86_common/mod.rs
+++ src/arch/x86_common/mod.rs
@@ -1,3 +1,11 @@
+use ::xen::StartInfo;
+
 pub mod defs;
 pub mod barrier;
 pub mod memory;
+
+extern {
+    // Start info is not present on all architecture, this is why this
+    // was made a global variable only for x86_*
+    pub static start_info: *const StartInfo;
+}
