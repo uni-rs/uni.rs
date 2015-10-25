9f3aee0b3ba3a27d43b8b0767da1c4f0b8e72b92
Baptiste Covolato <b.covolato@gmail.com>
2015-10-25 16:03:11 +0100

    x86/x86_64: Set stack size from rust

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,13 @@ extern {
     fn main(_: isize, _: *const *const u8) -> isize;
 }
 
+// 8KB
+const STACK_SIZE: usize = 8192;
+
 #[no_mangle]
 #[allow(non_upper_case_globals)]
 #[link_section=".stack"]
-pub static rust_stack: [u8; 8192] = [0; 8192];
+pub static rust_stack: [u8; STACK_SIZE] = [0; STACK_SIZE];
 
 #[no_mangle]
 pub fn uni_rust_entry() {
