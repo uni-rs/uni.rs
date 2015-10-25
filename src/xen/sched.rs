625ad080ab51ece8ab297f0519c78be5e819265a
Baptiste Covolato <b.covolato@gmail.com>
2015-10-25 21:13:44 +0100

    sched: Add sched_op hypercall wrapper

---
+++ src/xen/sched.rs
@@ -0,0 +1,20 @@
+use xen::hypercall::hypercall2;
+use xen::hypercall::HyperCalls;
+
+use ::arch::defs::Ulong;
+
+#[allow(dead_code)]
+enum SchedOp {
+    Yield = 0,
+    Block = 1,
+    Shutdown = 2,
+    Poll = 3,
+    RemoteShutdown = 4,
+    ShutdownCode = 5,
+    Watchdog = 6,
+}
+
+#[allow(dead_code)]
+fn sched_op(op: SchedOp, sched: Ulong) -> i32 {
+    hypercall2(HyperCalls::SchedOp, op as Ulong, sched) as i32
+}
