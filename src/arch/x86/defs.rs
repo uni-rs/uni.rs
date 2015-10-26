4bf4547f768b7735e0c4fdfbdabeed075efbfdf9
Baptiste Covolato <b.covolato@gmail.com>
2015-10-27 00:35:55 +0100

    x86/x86_64: Add definition of HYPERVISOR_START

--- src/arch/x86/defs.rs
+++ src/arch/x86/defs.rs
@@ -1,7 +1,11 @@
 //! Definitions for the x86 architecture
 
+use ::arch::x86_common::memory::Vaddr;
+
 pub use ::arch::x86_common::defs::*;
 
-pub const MACH2PHYS_VIRT_START: Ulong = 0xF5800000;
+pub const MACH2PHYS_VIRT_START: Vaddr = 0xF5800000;
 
 pub const XENULONGSIZE: usize = 4;
+
+pub const HYPERVISOR_START: Vaddr = 0xF5800000;
--- src/arch/x86_64/defs.rs
+++ src/arch/x86_64/defs.rs
@@ -1,7 +1,11 @@
 //! Definitions for the x86_64 architecture
 
+use ::arch::x86_common::memory::Vaddr;
+
 pub use ::arch::x86_common::defs::*;
 
-pub const MACH2PHYS_VIRT_START: Ulong = 0xFFFF800000000000;
+pub const MACH2PHYS_VIRT_START: Vaddr = 0xFFFF800000000000;
 
 pub const XENULONGSIZE: usize = 8;
+
+pub const HYPERVISOR_START: Vaddr = 0xFFFF800000000000;
