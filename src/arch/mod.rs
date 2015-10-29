278f51c8e9dfa5781667c8bb079185ead64c21f3
Baptiste Covolato <b.covolato@gmail.com>
2015-10-29 20:59:07 +0100

    arch: Add module compiler

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -4,6 +4,7 @@ pub mod x86_common;
 #[cfg(target_arch = "x86_64")]
 #[path="x86_64"]
 mod imp {
+    pub mod compiler {}
     pub mod defs;
     pub mod xen;
     pub use ::arch::x86_common::barrier;
@@ -13,14 +14,18 @@ mod imp {
 #[cfg(target_arch = "x86")]
 #[path="x86"]
 mod imp {
+    pub mod compiler {}
     pub mod defs;
     pub mod xen;
     pub use ::arch::x86_common::barrier;
     pub use ::arch::x86_common::init;
 }
 
+// Exported functions
 pub use self::imp::init;
 
+// Exported modules
 pub use self::imp::defs;
 pub use self::imp::xen;
 pub use self::imp::barrier;
+pub use self::imp::compiler;
