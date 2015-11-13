a31ec0f5745fc2451efcca0230ce9a22cae279b3
Baptiste Covolato <b.covolato@gmail.com>
2015-11-13 14:05:27 +0100

    heap: Add Allocator trait

--- lib/heap/src/lib.rs
+++ lib/heap/src/lib.rs
@@ -1,3 +1,26 @@
 #![feature(no_std)]
 
 #![no_std]
+
+use core::ptr;
+use core::cmp;
+
+pub trait Allocator {
+    unsafe fn allocate(&mut self, size: usize, align: usize) -> *mut u8;
+    unsafe fn deallocate(&mut self, ptr: *mut u8, old_size: usize, align: usize);
+
+    unsafe fn reallocate(&mut self, ptr: *mut u8, old_size: usize, size: usize,
+                  align: usize) -> *mut u8 {
+        let new_ptr = self.allocate(size, align);
+
+        if new_ptr.is_null() {
+            return ptr::null_mut();
+        }
+
+        ptr::copy(ptr, new_ptr, cmp::min(size, old_size));
+
+        self.deallocate(ptr, old_size, align);
+
+        new_ptr
+    }
+}
