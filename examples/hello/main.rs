29ab1788a273d2521f4b11ef34faee33253e43da
Baptiste Covolato <b.covolato@gmail.com>
2015-11-24 18:41:15 +0100

    examples: Hello now uses String

--- examples/hello/main.rs
+++ examples/hello/main.rs
@@ -5,9 +5,17 @@
 #[macro_use]
 extern crate uni;
 
+use uni::string::String;
+
 #[start]
 fn main(_: isize, _: *const *const u8) -> isize {
-    println!("Hello World");
+    let mut s = String::new();
+
+    s.push_str("Hello");
+    s.push_str(" ");
+    s.push_str("World");
+
+    println!("{}", s);
 
     0
 }
