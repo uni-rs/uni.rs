39dad562cdf0c427c67a838b797cf791d580b598
Baptiste Covolato <b.covolato@gmail.com>
2015-11-26 19:33:14 +0100

    libxen: Add enable_upcalls and disable_upcalls

--- src/libxen/src/lib.rs
+++ src/libxen/src/lib.rs
@@ -21,3 +21,26 @@ extern "C" {
     // This symbol must be present in code using libxen
     pub static mut shared_info: self::defs::SharedInfo;
 }
+
+pub fn enable_upcalls() -> u8 {
+    unsafe {
+        let ret = shared_info.vcpu_info[0].evtchn_upcall_mask;
+
+        self::intrinsics::wmb();
+        shared_info.vcpu_info[0].evtchn_upcall_mask = 0;
+        self::intrinsics::wmb();
+
+        ret
+    }
+}
+
+pub fn disable_upcalls() -> u8 {
+    unsafe {
+        let ret = shared_info.vcpu_info[0].evtchn_upcall_mask;
+
+        shared_info.vcpu_info[0].evtchn_upcall_mask = 1;
+        self::intrinsics::wmb();
+
+        ret
+    }
+}
