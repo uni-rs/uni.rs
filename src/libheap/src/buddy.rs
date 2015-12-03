35a4d12b8bcbace9d18713582b5554c46db74d70
Baptiste Covolato <b.covolato@gmail.com>
2015-12-03 14:07:03 +0000

    libheap: Add dummy implementation of Allocator for Buddy

--- src/libheap/src/buddy.rs
+++ src/libheap/src/buddy.rs
@@ -1,5 +1,9 @@
 //! An implementation of a buddy allocator.
 
+use core::ptr;
+
+use Allocator;
+
 use types::{UnsafeList, PhantomNode};
 
 pub type FreeBlock = PhantomNode;
@@ -9,4 +13,15 @@ pub struct Buddy<'a> {
     min_block_size: usize,
     max_order: u32,
     free_lists: &'a mut [UnsafeList<FreeBlock>],
+}
+
+impl<'a> Allocator for Buddy<'a> {
+    unsafe fn allocate(&mut self, mut _size: usize, _align: usize) -> *mut u8 {
+        ptr::null_mut()
+    }
+
+    unsafe fn deallocate(&mut self, _ptr: *mut u8, _old_size: usize,
+                         _align: usize) {
+
+    }
 }
