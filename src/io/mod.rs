f98a3b1147a727e62c50b9c5d90473958d3847a0
Baptiste Covolato <b.covolato@gmail.com>
2015-12-18 16:03:41 +0000

    io: Add write_fmt() to Write

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,5 +1,6 @@
 //! Definition of types, traits, ... for I/O functionality
 
+use core::fmt;
 use core::result;
 
 pub type Result<T> = result::Result<T, ()>;
@@ -11,4 +12,28 @@ pub trait Read {
 pub trait Write {
     fn write(&mut self, buf: &[u8]) -> Result<usize>;
     fn flush(&mut self) -> Result<()>;
+
+    fn write_fmt(&mut self, fmt: fmt::Arguments) -> Result<()> {
+        struct Adaptor<'a, T: ?Sized + 'a> {
+            inner: &'a mut T,
+        }
+
+        impl<'a, T: ?Sized + Write> fmt::Write for Adaptor<'a, T> {
+            fn write_str(&mut self, s: &str) -> fmt::Result {
+                match self.inner.write(s.as_bytes()) {
+                    Ok(..) => Ok(()),
+                    Err(..) => Err(fmt::Error),
+                }
+            }
+        }
+
+        let mut tmp = Adaptor {
+            inner: self,
+        };
+
+        match fmt::write(&mut tmp, fmt) {
+            Ok(..) => Ok(()),
+            Err(..) => Err(()),
+        }
+    }
 }
