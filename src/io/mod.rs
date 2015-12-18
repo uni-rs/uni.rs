cf66aac05db2c3b25a464d3e2413ef8e81eed9bd
Baptiste Covolato <b.covolato@gmail.com>
2015-12-18 15:27:07 +0000

    io: Add traits Read and Write

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,1 +1,14 @@
 //! Definition of types, traits, ... for I/O functionality
+
+use core::result;
+
+pub type Result<T> = result::Result<T, ()>;
+
+pub trait Read {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize>;
+}
+
+pub trait Write {
+    fn write(&mut self, buf: &[u8]) -> Result<usize>;
+    fn flush(&mut self) -> Result<()>;
+}
