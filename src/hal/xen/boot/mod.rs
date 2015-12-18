88210b755b0585940780daf4da15e75f0a1312a1
Baptiste Covolato <b.covolato@gmail.com>
2015-12-18 14:41:42 +0000

    hal: Move module xen/boot/arch to xen/boot

--- src/hal/xen/boot/mod.rs
+++ src/hal/xen/boot/mod.rs
@@ -1,63 +1,14 @@
+use hal::defs::PAGE_SIZE;
+
+mod x86;
 pub mod event;
-pub mod arch;
 
-use hal::{local_irq_enable, local_irq_disable};
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+pub use self::x86::*;
 
-extern {
-    fn main(_: isize, _: *const *const u8) -> isize;
-}
-
-use thread::Scheduler;
-
-// 8KB
-const STACK_SIZE: usize = 8192;
+const STACK_SIZE: usize = 2 * PAGE_SIZE;
 
 #[no_mangle]
 #[allow(non_upper_case_globals)]
 #[link_section=".stack"]
 pub static rust_stack: [u8; STACK_SIZE] = [0; STACK_SIZE];
-
-#[no_mangle]
-pub extern "C" fn uni_rust_entry() -> ! {
-    self::arch::init();
-
-    println!("Uni.rs is booting");
-
-    // Memory initialization is unsafe
-    unsafe {
-        let (heap_start, heap_size) = arch::init_memory();
-
-        ::allocator::init(heap_start, heap_size);
-    }
-
-    event::init();
-
-    unsafe {
-        ::console::console().init_input();
-    }
-
-    local_irq_enable();
-
-    println!("Creating main thread");
-
-    // Spawn main thread
-    Scheduler::spawn(|| {
-        let app_ret = unsafe {
-            main(0, ::core::ptr::null())
-        };
-
-        local_irq_disable();
-
-        ::console::console().flush();
-
-        ::hal::xen::sched::poweroff(app_ret as ::hal::xen::defs::Ulong);
-
-        panic!("Failed to poweroff the machine !");
-    });
-
-    println!("Starting scheduler");
-
-    Scheduler::schedule();
-
-    unreachable!();
-}
--- src/hal/xen/mod.rs
+++ src/hal/xen/mod.rs
@@ -1,4 +1,11 @@
+use hal::{
+    local_irq_enable,
+    local_irq_disable
+};
+
 use hal::intrinsics::wmb;
+
+use thread::Scheduler;
 
 mod hypercall;
 
@@ -47,4 +54,54 @@ pub fn set_upcalls_state(state: u8) {
         shared_info.vcpu_info[0].evtchn_upcall_mask = state;
         wmb();
     }
+}
+
+extern {
+    fn main(_: isize, _: *const *const u8) -> isize;
+}
+
+#[no_mangle]
+/// Entry point of the application called by boot assembly
+pub extern "C" fn uni_rust_entry() -> ! {
+    boot::init();
+
+    println!("Uni.rs is booting");
+
+    // Memory initialization is unsafe
+    unsafe {
+        let (heap_start, heap_size) = boot::init_memory();
+
+        ::allocator::init(heap_start, heap_size);
+    }
+
+    boot::event::init();
+
+    unsafe {
+        ::console::console().init_input();
+    }
+
+    local_irq_enable();
+
+    println!("Creating main thread");
+
+    // Spawn main thread
+    Scheduler::spawn(|| {
+        let app_ret = unsafe {
+            main(0, ::core::ptr::null())
+        };
+
+        local_irq_disable();
+
+        ::console::console().flush();
+
+        sched::poweroff(app_ret as ::hal::xen::defs::Ulong);
+
+        panic!("Failed to poweroff the machine !");
+    });
+
+    println!("Starting scheduler");
+
+    Scheduler::schedule();
+
+    unreachable!();
 }
