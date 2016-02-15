514e5c55e5b2ad79769ead45cdc2680c610ec71b
Baptiste Covolato <b.covolato@gmail.com>
2016-02-15 20:55:41 +0000

    net/conn: Add definition of type EtherType, ProtocolIdType and PortType

--- src/net/conn/defs.rs
+++ src/net/conn/defs.rs
@@ -1,1 +1,8 @@
 //! Definition of various types related to connexions
+
+/// Type of the ether_type
+pub type EtherType = u16;
+/// Type that represent a protocol id
+pub type ProtocolIdType = u8;
+/// Type that represent a port
+pub type PortType = u16;
