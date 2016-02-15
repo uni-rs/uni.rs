ccc0f3014aa514803a27c1bde25b54c165c1ae87
Baptiste Covolato <b.covolato@gmail.com>
2016-02-15 21:26:04 +0000

    net/conn: Add type EthernetRule, NetworkRule and TransportRule

--- src/net/conn/defs.rs
+++ src/net/conn/defs.rs
@@ -1,8 +1,27 @@
 //! Definition of various types related to connexions
 
+use net::defs::{HwAddr, IpAddr};
+
 /// Type of the ether_type
 pub type EtherType = u16;
 /// Type that represent a protocol id
 pub type ProtocolIdType = u8;
 /// Type that represent a port
 pub type PortType = u16;
+
+/// Ethernet layer part of the rule
+pub struct EthernetRule {
+    pub ether_type: EtherType,
+    pub hw_src: Option<HwAddr>,
+}
+
+/// Network layer part of the rule
+pub struct NetworkRule {
+    pub protocol_id: ProtocolIdType,
+    pub ip_src: Option<IpAddr>,
+}
+
+/// Transport layer part of the rule
+pub struct TransportRule {
+    pub port: PortType,
+}
