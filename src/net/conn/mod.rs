4d74c1a96e9e4c7690f585fcf9c625647a2d4942
Baptiste Covolato <b.covolato@gmail.com>
2016-02-20 12:57:59 +0000

    net/conn: Add enum Connexion

--- src/net/conn/mod.rs
+++ src/net/conn/mod.rs
@@ -1,7 +1,15 @@
 //! Contains various type and trait definition related to connexion management
 
+use sync::Arc;
+
 mod uni;
 mod multi;
 
 pub use self::uni::UniConn;
 pub use self::multi::MultiConn;
+
+/// A connexion, either uni or multi
+pub enum Connexion {
+    Uni(Arc<UniConn>),
+    Multi(Arc<MultiConn>),
+}
