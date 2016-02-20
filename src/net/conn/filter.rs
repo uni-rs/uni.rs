ce41c345f09395ee170dbc76322b9709cf987759
Baptiste Covolato <b.covolato@gmail.com>
2016-02-20 14:19:44 +0000

    conn/filter: Add definition of trait PacketSanitizer

--- src/net/conn/filter.rs
+++ src/net/conn/filter.rs
@@ -4,3 +4,21 @@
 //!
 //! - Generic parameter: ether type, protocol id, ...
 //! - Specific parameter: hardware address, ip address, ...
+
+use net::Packet;
+
+/// Sanitize an incoming packet.
+///
+/// Packets that arrive from the network need to be checked by every used
+/// protocols for correctness (i.e. checksum, ...).
+///
+/// Some properties or extra information might need to be added to the packet
+/// as well.
+///
+/// This trait let protocols do this.
+pub trait PacketSanitizer {
+    /// This method is called when a filter needs to sanitize a packet.
+    ///
+    /// If this method returns an `Err` the packet will be dropped.
+    fn sanitize(pkt: &mut Packet) -> Result<(), ()>;
+}
