68f09575d0e9390646d2ef1c8389ba9b6eb4e6f7
Baptiste Covolato <b.covolato@gmail.com>
2016-02-20 14:25:43 +0000

    conn/filter: Add definition of trait Extractor

--- src/net/conn/filter.rs
+++ src/net/conn/filter.rs
@@ -7,6 +7,25 @@
 
 use net::Packet;
 
+use net::defs::Rule;
+
+/// Extract generic/specific parameters of type T from a packet or a rule.
+///
+/// This is used by filters to extract the generic/specific parameter and use
+/// it to route the packet to the appropriate connexion (if such connexion
+/// exists).
+pub trait Extractor<T> {
+    /// Extract the parameter from a rule.
+    ///
+    /// This will return `None` if the method fails to extract the parameter.
+    fn from_rule(rule: &Rule) -> Option<T>;
+
+    /// Extract the parameter from a Packet.
+    ///
+    /// This will return `None` if the method fails to extract the parameter.
+    fn from_packet(pkt: &Packet) -> Option<T>;
+}
+
 /// Sanitize an incoming packet.
 ///
 /// Packets that arrive from the network need to be checked by every used
