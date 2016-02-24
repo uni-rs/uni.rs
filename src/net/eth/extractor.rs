82e1c5672a2e1ab101bbd672cce61b6ebe9841f8
Baptiste Covolato <b.covolato@gmail.com>
2016-02-24 21:47:43 +0000

    net/eth: Add definition of type SourceHwExtractor

--- src/net/eth/extractor.rs
+++ src/net/eth/extractor.rs
@@ -2,7 +2,7 @@
 
 use net::Packet;
 
-use net::defs::{Rule, EtherType};
+use net::defs::{Rule, HwAddr, EtherType};
 
 use net::conn::filter::Extractor;
 
@@ -20,5 +20,20 @@ impl Extractor<EtherType> for EtherTypeExtractor {
     /// Extract the ether type from a packet
     fn from_packet(pkt: &Packet) -> Option<EtherType> {
         pkt.link_header::<Header>().map(|hdr| hdr.ether_type.as_host())
+    }
+}
+
+/// Type responsible for source hardware extraction
+pub struct SourceHwExtractor;
+
+impl Extractor<HwAddr> for SourceHwExtractor {
+    /// Extract the source hardware address from a rule
+    fn from_rule(_rule: &Rule) -> Option<HwAddr> {
+        unimplemented!();
+    }
+
+    /// Extract the source hardware address from a packet
+    fn from_packet(pkt: &Packet) -> Option<HwAddr> {
+        pkt.link_header::<Header>().map(|hdr| hdr.src.clone())
     }
 }
