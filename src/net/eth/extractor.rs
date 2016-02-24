a77564a3ff2084a3dc129cc476280050ae1d48d5
Baptiste Covolato <b.covolato@gmail.com>
2016-02-24 21:12:58 +0000

    net/eth: Add definition of type EtherTypeExtractor

--- src/net/eth/extractor.rs
+++ src/net/eth/extractor.rs
@@ -1,1 +1,24 @@
 //! Implementation of Ethernet related Extractors
+
+use net::Packet;
+
+use net::defs::{Rule, EtherType};
+
+use net::conn::filter::Extractor;
+
+use super::defs::Header;
+
+/// Type responsible for ether type extraction
+pub struct EtherTypeExtractor;
+
+impl Extractor<EtherType> for EtherTypeExtractor {
+    /// Extract the ether type from a rule
+    fn from_rule(rule: &Rule) -> Option<EtherType> {
+        rule.eth_rule.as_ref().map(|eth_rule| eth_rule.ether_type)
+    }
+
+    /// Extract the ether type from a packet
+    fn from_packet(pkt: &Packet) -> Option<EtherType> {
+        pkt.link_header::<Header>().map(|hdr| hdr.ether_type.as_host())
+    }
+}
